db6ed15e31dc95815c2adf604bce0fd870f230a0
Spazzy <brendankamp757@gmail.com>
2021-04-08 11:52:42 +0200

    Added setup for vue and updated git config

--- doom/config.el
+++ doom/config.el
@@ -136,6 +136,15 @@ or the current buffer directory."
   :init
   (add-hook 'sh-mode-hook 'flymake-shellcheck-load))
 
+;; -- Vue --
+(require 'eglot)
+(require 'web-mode)
+(define-derived-mode genehack-vue-mode web-mode "ghVue"
+  "A major mode derived from web-mode, for editing .vue files with LSP support.")
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . genehack-vue-mode))
+(add-hook 'genehack-vue-mode-hook #'eglot-ensure)
+(add-to-list 'eglot-server-programs '(genehack-vue-mode "vls"))
+
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
