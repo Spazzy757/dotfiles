3f7a68d6724bc096bc2dd49fdda0ca427eacb0ad
Spazzy <brendankamp757@gmail.com>
2025-01-21 08:29:37 +0100

    chore: update dotfiles
    Signed-off-by: Spazzy <brendankamp757@gmail.com>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -55,4 +55,11 @@ return {
       'ray-x/guihua.lua',
     },
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    opts = {},
+ }
 }
